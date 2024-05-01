296ded61cc4ab51cf70e328146095a085ee6b89f
André Ghisleni Raimann <andre@andreg.com.br>
2024-05-01 19:06:03 -0300

    Changes to be committed: 	modified:   apps/web/src/app/api/astrophotographies/term-of-use/route.ts

--- apps/web/src/app/api/astrophotographies/term-of-use/route.ts
+++ apps/web/src/app/api/astrophotographies/term-of-use/route.ts
@@ -3,7 +3,7 @@ export const dynamic = 'force-dynamic'
 
 export async function GET() {
   return NextResponse.redirect(
-    'https://docs.google.com/document/d/1aLtcHduXXZ4JJFMmX_Iuw__pwZb8BKLp/edit?usp=sharing&ouid=100877972451705059128&rtpof=true&sd=true',
+    'https://drive.google.com/uc?export=download&id=1aLtcHduXXZ4JJFMmX_Iuw__pwZb8BKLp',
     {
       headers: {
         'Access-Control-Allow-Origin': '*',
